79decbd44442c5873f8883b98e4c026526fc12ca
Naum Markenzon <nmarkenz@ucsc.edu>
2019-01-19 12:13:22 -0800

    Form halfway done + comments

--- PinkyPromiseProject/App.js
+++ PinkyPromiseProject/App.js
@@ -2,19 +2,62 @@ import React , {Component} from 'react';
 import { Image, Platform, Text, StatusBar,StyleSheet, View } from 'react-native';
 import { AppLoading, Asset, Font, Icon } from 'expo';
 import AppNavigator from './navigation/AppNavigator';
+import t from 'tcomb-form-native';
+
+//  This Form is the variable that we use to instantiate the form
+const Form = t.form.Form;
+
+//  Basic information that we are collecting from the users
+const User = t.struct({
+  email: t.String,
+  username: t.String,
+  password: t.String,
+  terms: t.Boolean
+});
+
+export default class App extends Component {
+  /*
+    Function used to handle the data submitted when making a profile
+  */
+  handleSubmit = () => {
+    // do the things  
+  }
+
+  //  Rendering the App (How it looks like on your phone)
+
+  render() {
+    let pic = {
+      uri: 'https://images.pexels.com/photos/1389098/pexels-photo-1389098.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
+    };
+    return (
+      <View style= {styles.container}>
+      <Form type={User} />
+      <Image source={pic} style={{width: 253, height: 160}}/>
+        <Text style = {styles.welcome}>Pinky Promise!</Text>
+      </View>
+    );
+  }
+}
+
 
 /*
 VISUAL COMPONENT OF CODE
+These styles are used to customize the layout of our text
 */
 
 const styles = StyleSheet.create({
+  container: {
+    justifyContent: "center",
+    padding: 20,
+    backgroundColor: "#FFB6C1",
+  },
   bigblue: {
     color: 'blue',
     fontWeight: 'bold',
     fontSize: 30,
   },
   welcome: {
-    color: 'pink',
+    color: 'white',
     fontWeight: 'bold',
     fontSize: 30,
     fontFamily: "MarkerFelt-Wide",
@@ -25,17 +68,3 @@ const styles = StyleSheet.create({
     color: 'red',
   },
 });
-
-export default class App extends Component {
-  render() {
-    let pic = {
-      uri: 'https://images.pexels.com/photos/1389098/pexels-photo-1389098.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
-    };
-    return (
-      <View>
-        <Image source={pic} style={{width: 253, height: 160}}/>
-        <Text style = {styles.welcome}>Pinky Promise!</Text>
-      </View>
-    );
-  }
-}
